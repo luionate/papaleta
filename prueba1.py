021dc602464ab4cd028628a25596d0fbbd6095da
user-655 <lui.onate@duocuc.cl>
2026-06-17 15:40:51 -0400

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -1,4 +1,17 @@
 #FUNCONES
+#validaciones
+def validar_nombre(name):
+    #una funcion que elimina los espacions el inicaio y al final de un string y si queda vacia devuelve un false
+    return name.strip() != "" #Retorna True si es valido- False si es invalido 
+def validar_especie(especie):
+    #Verificr que es un perro, gato o ave solamente(Sin diferenciar mayusculas o minusculas)
+    especies_validad = ["perro","gato","ave"]
+    return especie.strip().lower() in especies_validad
+def validar_edad(edad):
+    #que sean numeros y mayor a 0
+    #isdigit() -> revisa que el strig contenga solo digitos(no negativo, no decimal)
+    return edad.isdigit() and int(edad) > 0
+     
 def mostrar_menu():
     print("======MENÚ PRINCIPAL========")
     print("1.- Agregar mascota")
@@ -19,24 +32,51 @@ def ingresar_opcion():
         except ValueError:
             print("Debe ingresar un número")
     return opcion
+#Funcion para agregar mascota
+def agregar_mascota(lista):
+    nombre = input("Ingrese el nombre de la mascota: ")
+    #llamar la funcion que valida nombre para mostrar el mesaje
+    correcto = validar_nombre(nombre)
+    if not correcto:
+        print("El nombre no puede estar vacío")
+        return
+    especie = input("Ingrese la especie de la mascota (Perro, Gato o Ave): ")
+    correcto = validar_especie(especie)
+    if not correcto:
+        print("La especie debe ser Perro, Gato o Ave")
+        return
+    edad = input("Ingrese la edad de la mascota")
+    correcto = validar_edad(edad)
+    if not correcto:
+        print("La edad debe ser un numero entero y mayor a cero")
+        return
+    #aqui agrego el diccionario
+    mascota ={
+        "nombre": nombre.strip(),
+        "especie": especie.strip(),
+        "edad": int(edad),
+        "vacunada": False
+    }
+    lista.append(mascota)
+    print("Mascota agregada correctamente")
+    
 #CODIGO PRINCIPAL
 #declaro la lista de mascota
+
 lista_mascotas = []
 op = 0
 while op != 6:
     mostrar_menu()
     op = ingresar_opcion()
-
-
-
-
-
-if op = 1:
-elif op = 2:
-elif op = 3:
-elif op = 4:
-elif op = 5:
-elif op = 6:
-
-
-    
+    if op == 1:
+        agregar_mascota(lista_mascotas)
+    elif op == 2:
+        print()
+    elif op == 3:
+        print()
+    elif op == 4:
+        print()
+    elif op == 5:
+        print()
+    elif op == 6:
+        print("Gracias por usar el sistema")
